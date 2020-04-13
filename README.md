fd53367ffa9b359616592925fe0ba599853cf1b3
Héctor Villarroel <hvillarroel@synaptic.cl>
2020-04-12 21:09:43 -0400

    Update with doc in README.md

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# simple-shiro-permissions
+# Simple Shiro Permissions
 It is a simple module based in apache Shiro style.
+
+
+# Install
+
+```bash
+npm install simple-shiro-permissions
+```
+
+## How to use
+
+List permissions
+
+```js
+const { getPermissions, check } = require('./index')
+const permissions = [
+    'loanFlow:view',
+    'loanFlow:review',
+    'loanFlow:create',
+    'user:view',
+    'user:create',
+    'user:update',
+    'user:delete'
+]
+
+getPermissions(permissions, 'loanFlow:view')
+// Result
+// ['loanFlow:view']
+check(permissions, 'loanFlow:view')
+// Result
+// True
+
+getPermissions(permissions, 'loanFlow:view,review')
+// Result
+// ['loanFlow:view', 'loanFlow:review']
+check(permissions, 'loanFlow:view,review')
+// Result
+// true
+
+getPermissions(permissions, 'loanFlow:*')
+// Result
+// ['loanFlow:view', 'loanFlow:review', 'loanFlow:create']
+
+getPermissions(permissions, '*:*')
+// Result
+// [
+//   'loanFlow:view',
+//   'loanFlow:review',
+//   'loanFlow:create',
+//   'user:view',
+//   'user:create',
+//   'user:update',
+//   'user:delete'
+// ]
+
+getPermissions(permissions, '*:view')
+// Result
+// ['loanFlow:view', 'user:view']
+
+getPermissions(permissions, '*:view,create')
+// Result
+// ['loanFlow:view', 'loanFlow:create', 'user:view', 'user:create']
+
+```
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const { getPermissions } = require('./index')
+const { getPermissions, check } = require('./index')
 
 const permissions = [
     'loanFlow:view',
@@ -11,7 +11,7 @@ const permissions = [
 ]
 
 
-describe('Permission valid Format', () => {
+describe('Get Permissions', () => {
   it('It should valid `loanFlow:view`', (done) => {
     expect(getPermissions(permissions, 'loanFlow:view')).toStrictEqual(['loanFlow:view'])
     return done()
@@ -41,12 +41,49 @@ describe('Permission valid Format', () => {
   })
 
   it('It should valid `*:view`', (done) => {
-    expect(getPermissions(permissions, '*:view')).toStrictEqual(['loanFlow:view', 'user:view',])
+    expect(getPermissions(permissions, '*:view')).toStrictEqual(['loanFlow:view', 'user:view'])
     return done()
   })
 
   it('It should valid `*:view,read`', (done) => {
     expect(getPermissions(permissions, '*:view,create')).toStrictEqual(['loanFlow:view', 'loanFlow:create', 'user:view', 'user:create'])
+    return done()
+  })
+})
+
+describe('Check Permissions', () => {
+  it('It should valid `loanFlow:view`', (done) => {
+    expect(check(permissions, 'loanFlow:view')).toBeTruthy()
+    return done()
+  })
+  
+  it('It should valid `loanFlow:view,review`', (done) => {
+    expect(check(permissions, 'loanFlow:view,review')).toBeTruthy()
+    return done()
+  })
+
+  it('It should valid `loanFlow:*`', (done) => {
+    expect(check(permissions, 'loanFlow:*')).toBeTruthy()
+    return done()
+  })
+
+  it('It should valid `*:*`', (done) => {
+    expect(check(permissions, '*:*')).toBeTruthy()
+    return done()
+  })
+
+  it('It should valid `*:view`', (done) => {
+    expect(check(permissions, '*:view')).toBeTruthy()
+    return done()
+  })
+
+  it('It should valid `*:view,read`', (done) => {
+    expect(check(permissions, '*:view,create')).toBeTruthy()
+    return done()
+  })
+
+  it('It should valid `*:*,read`', (done) => {
+    expect(check(permissions, '*:*,create')).toBeFalsy()
     return done()
   })
 })
